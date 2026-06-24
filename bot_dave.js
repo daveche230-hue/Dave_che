880182f8ac413af32e9a1e9e69dd82240065042e
daveche230-hue <daveche230@gmail.com>
2026-06-24 21:38:00 +0200

    Add files via upload

---
+++ bot_dave.js
@@ -0,0 +1,40 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+    host: 'mc.mineblaze.net',
+    port: 25565,
+    username: 'Dave_che',
+    version: '1.20.1'
+});
+
+bot.once('spawn', () => {
+    console.log("Бот Dave_che запущен в автономном режиме");
+    
+    // Авто-логин
+    setTimeout(() => {
+        bot.chat('/login 007007007');
+    }, 6000);
+
+    // Авто-вход в клан
+    setTimeout(() => {
+        bot.chat('/c join Eternia');
+    }, 10000);
+
+    // Автономная реклама каждые 3 минуты (180 000 миллисекунд)
+    setInterval(() => {
+        const adText = "&4&lнабираем бойцов в боевое крыло eternia тренировки тактика стрельба спарринги четкая система рангов и прогресс только для тех кто готов к дисциплине и серьезным замесам  warp ArmiaEternia";
+        bot.chat(adText);
+    }, 180000);
+});
+
+// Переподключение при кике
+bot.on('kicked', (reason) => {
+    console.log("Кикнут, перезапуск через 5 секунд...");
+    setTimeout(() => {
+        process.exit(1); // Хостинг сам перезапустит бота
+    }, 5000);
+});
+
+bot.on('error', (err) => {
+    console.log("Ошибка: ", err);
+});
